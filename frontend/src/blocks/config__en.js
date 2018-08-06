72d4407ccc1d9a0a429c6bde7ad29285bb4cf18d
mmserran <MarkAnthony.Serrano2@gmail.com>
2018-08-06 03:55:44 -0700

    create sitedown page for mobile/tablet/desktop.

--- frontend/src/blocks/config__en.js
+++ frontend/src/blocks/config__en.js
@@ -2,5 +2,7 @@ angular.module("monterey-signs.blocks")
         .config(function ($translateProvider) {
             $translateProvider.translations("en", {
                 "": ""
+        
+                , "sitedown_404": "Oops! This page is not yet available."
             });
         });
