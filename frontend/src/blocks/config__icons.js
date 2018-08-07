40c77bc5f393624886ac502b8e98986921bc25bf
mmserran <MarkAnthony.Serrano2@gmail.com>
2018-08-07 01:46:49 -0700

    add icons and implement footer layout.

--- frontend/src/blocks/config__icons.js
+++ frontend/src/blocks/config__icons.js
@@ -1,4 +1,7 @@
 angular.module("monterey-signs.blocks")
         .config(function ($mdIconProvider) {
             $mdIconProvider
+                    .iconSet('social:instagram', 'resources/feather/instagram.svg', 24)
+                    .iconSet('social:facebook', 'resources/feather/facebook.svg', 24)
+                    .iconSet('social:yelp', 'resources/feather/loader.svg', 24)
         });
